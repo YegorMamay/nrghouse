f7c224e0fbad7ed7a5314b7bf8967e49cf7e1e2a
Pavel <ponomarenko.pavel.web@gmail.com>
2020-08-10 18:15:34 +0300

    fix: tabs content for mobile ver

--- page-home.php
+++ page-home.php
@@ -82,33 +82,40 @@
                     $tab_list = get_field('services_tabs');
                     $tab_count = 0;
                     $tab_content = 0;
+                    $mobile_content = 0;
                     ?>
                     <?php foreach ($tab_list as $list_item) { ?>
                         <button data-tab="#tab<?php echo $tab_count; ?>"
                                 class="services-tabs__item<?php if ($tab_count == 0) { ?> active<?php } ?>"
                                 type="button"><?php echo $list_item['services_tabs_title']; ?></button>
-                        <div class="tab-mobile<?php if ($tab_count == 0) { ?> active<?php } ?>">
-                            <div class="tab-mobile__container">
-                                <div class="services-content__description-block">
-                                    <img class="services-content__icon"
-                                         src="/wp-content/themes/nrghouse/assets/img/quality.svg" alt="icon"/>
-                                    <div class="services-content__description"><?php echo $list_item['services_tabs_content']['services_tabs_description']; ?></div>
-                                </div>
-                                <div class="tab-mobile__image-wrapper">
-                                    <img class="tab-mobile__image"
-                                         src="<?php echo $list_item['services_tabs_content']['services_tabs_image']; ?>" alt="image">
-                                </div>
-                                <div class="tab-mobile__wrapper">
-                                    <p class="tab-mobile__title"><?php echo $list_item['services_tabs_title']; ?></p>
-                                    <div class="tab-mobile__description">
-                                        <div class="tab-mobile__text"><?php echo $list_item['services_tabs_content']['services_tabs_text']; ?></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <?php $tab_count++; ?>
                     <?php }
                     wp_reset_postdata(); ?>
+
+                    <div class="mobile-content">
+                        <?php foreach ($tab_list as $tab_mobile) { ?>
+                            <div class="tab-mobile<?php if ($mobile_content == 0) { ?> active<?php } ?>">
+                                <div class="tab-mobile__container">
+                                    <div class="services-content__description-block">
+                                        <img class="services-content__icon"
+                                             src="/wp-content/themes/nrghouse/assets/img/quality.svg" alt="icon"/>
+                                        <div class="services-content__description"><?php echo $tab_mobile['services_tabs_content']['services_tabs_description']; ?></div>
+                                    </div>
+                                    <div class="tab-mobile__image-wrapper">
+                                        <img class="tab-mobile__image"
+                                             src="<?php echo $tab_mobile['services_tabs_content']['services_tabs_image']; ?>" alt="image">
+                                    </div>
+                                    <div class="tab-mobile__wrapper">
+                                        <p class="tab-mobile__title"><?php echo $tab_mobile['services_tabs_title']; ?></p>
+                                        <div class="tab-mobile__description">
+                                            <div class="tab-mobile__text"><?php echo $tab_mobile['services_tabs_content']['services_tabs_text']; ?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php $mobile_content++; ?>
+                        <?php } wp_reset_postdata(); ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -180,7 +187,8 @@
 
             servicesTabs.on('click', function () {
 
-                var currentTab = $(this).next();
+                var currentTab = servicesMobileContent.eq($(this).attr('data-tab').replace('#tab', ''));
+                console.log(currentTab);
 
                 servicesTabs.not($(this)).removeClass('active');
                 $(this).toggleClass('active');
